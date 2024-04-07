53c1da5d9f934009a22959ef17fe7fdaa16bd561
Lorenzo <lorenzo.2022310934@aluno.iffar.edu.br>
2024-04-06 21:42:31 -0300

    login

--- login.php
+++ login.php
@@ -35,8 +35,6 @@ if (isset($_POST['login'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/jpg" href="img/icons8-rocha-48.png" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
     <title>Login</title>
     <style>
         .center {
