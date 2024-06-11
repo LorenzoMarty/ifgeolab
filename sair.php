045d3bc15d024c13c178e10129e1685d14820e1e
lorenzo <lorenzo@gmail.com>
2024-06-11 16:04:19 -0300

    commit

--- sair.php
+++
@@ -1,7 +0,0 @@
-<?php
-setcookie("acesso[usuario]", '', time() - 7200);
-setcookie("acesso[email]", '', time() -7200);
-setcookie("acesso[senha])", '', time() - 7200);
-setcookie("acesso[permissao]", '', time() - 7200);
-setcookie("acesso[id]", '', time() - 7200);
-header("Location:index.php");
