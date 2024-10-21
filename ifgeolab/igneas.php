f094d2c39fd47d677c522c183d739c0172562c75
lorenzo <lorenzo@gmail.com>
2024-10-21 09:34:39 -0300

    design

--- ifgeolab/igneas.php
+++ ifgeolab/igneas.php
@@ -12,6 +12,7 @@ include "include.php"; ?>
         width: 100%;
     }
 </style>
+<link rel="stylesheet" href="css/rocha-mineral.css">
 
 <body>
     <?php
@@ -23,45 +24,43 @@ include "include.php"; ?>
     navbar($breadcrumb);
     ?>
     <main>
-
-        <div class="container center">
-            <div class="row col s12">
-                <h3>Rochas Ígneas</h3>
-            </div>
-            <hr>
-        </div>
-
         <div class="container">
-            <div class="row">
-                <?php
-                require_once 'conecta.php';
-                $sql = "SELECT * FROM rocha WHERE idcat=1 and sugestao=0";
-                $conexao = conectar();
-                $resultado = mysqli_query($conexao, $sql);
-                while ($dados = mysqli_fetch_array($resultado)) {
-                    $nome = $dados['nome'];
-                    $cat = $dados['idcat'];
-                    $descricao = $dados['descricao'];
-                    $img = $dados['img'];
+            <div class="vertical-line"></div>
+            <div class="section-content">
+                <div class="section">
+                    <h4 class="left-align">Rochas Ígneas</h4>
+                    <hr class="divider">
+                </div>
+                <div class="row">
+                    <?php
+                    require_once 'conecta.php';
+                    $sql = "SELECT * FROM rocha WHERE idcat=1 and sugestao=0";
+                    $conexao = conectar();
+                    $resultado = mysqli_query($conexao, $sql);
+                    while ($dados = mysqli_fetch_array($resultado)) {
+                        $nome = $dados['nome'];
+                        $cat = $dados['idcat'];
+                        $descricao = $dados['descricao'];
+                        $img = $dados['img'];
 
-                ?>
-                    <div class="col s12 l4 m8">
-                        <div class="card hoverable">
+                    ?>
+                        <div class="col s12 l4 m8">
+                            <div class="card hoverable">
 
-                            <div class="card-image transparent">
-                                <img src="img/rochas/<?= $img; ?>" class="minha-imagem materialboxed ">
-                                <span class="card-title center meu-span green-text text-lighten-3"><?php echo $nome     ?></span>
-                            </div>
-                            <div class="card-action green darken-4">
-                                <a class="green-text text-lighten-3" href="saibaRocha.php?idrocha=<?php echo $dados['idrocha']  ?>">Saiba mais</a>
+                                <div class="card-image transparent">
+                                    <img src="img/rochas/<?= $img; ?>" class="minha-imagem materialboxed ">
+                                    <span class="card-title center meu-span green-text text-lighten-3"><?php echo $nome     ?></span>
+                                </div>
+                                <div class="card-action green darken-4">
+                                    <a class="green-text text-lighten-3" href="saibaRocha.php?idrocha=<?php echo $dados['idrocha']  ?>">Saiba mais</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                <?php } ?>
+                    <?php } ?>
+                </div>
             </div>
-        </div>
-        <br><br><br>
+            <br><br><br>
     </main>
     <?php
     include "footer.php";
--- ifgeolab/sed.php
+++ ifgeolab/sed.php
@@ -22,48 +22,47 @@ navbar($breadcrumb);
         width: 100%;
     }
 </style>
+<link rel="stylesheet" href="css/rocha-mineral.css">
 
 <body>
     <main>
-
-        <div class="container center">
-            <div class="row col s12">
-                <h3>Rochas Sedimentares</h3>
-            </div>
-            <hr>
-        </div>
-
         <div class="container">
-            <div class="row">
-                <?php
-                require_once 'conecta.php';
-                $sql = "SELECT * FROM rocha WHERE idcat=3 and sugestao=0";
-                $conexao = conectar();
-                $resultado = mysqli_query($conexao, $sql);
-                while ($dados = mysqli_fetch_array($resultado)) {
-                    $nome = $dados['nome'];
-                    $cat = $dados['idcat'];
-                    $descricao = $dados['descricao'];
-                    $img = $dados['img'];
+            <div class="vertical-line"></div>
+            <div class="section-content">
+                <div class="section">
+                    <h4 class="left-align">Rochas Sedimentares</h4>
+                    <hr class="divider">
+                </div>
+                <div class="row">
+                    <?php
+                    require_once 'conecta.php';
+                    $sql = "SELECT * FROM rocha WHERE idcat=3 and sugestao=0";
+                    $conexao = conectar();
+                    $resultado = mysqli_query($conexao, $sql);
+                    while ($dados = mysqli_fetch_array($resultado)) {
+                        $nome = $dados['nome'];
+                        $cat = $dados['idcat'];
+                        $descricao = $dados['descricao'];
+                        $img = $dados['img'];
 
-                ?>
-                    <div class="col s12 l4 m8">
-                        <div class="card hoverable">
+                    ?>
+                        <div class="col s12 l4 m8">
+                            <div class="card hoverable">
 
-                            <div class="card-image">
-                                <img src="img/rochas/<?= $img; ?>" class="minha-imagem materialboxed ">
-                                <span class="card-title center meu-span green-text text-lighten-3"><?php echo $nome     ?></span>
-                            </div>
-                            <div class="card-action green darken-4">
-                                <a class="green-text text-lighten-3" href="saibaRocha.php?idrocha=<?php echo $dados['idrocha']  ?>">Saiba mais</a>
+                                <div class="card-image">
+                                    <img src="img/rochas/<?= $img; ?>" class="minha-imagem materialboxed ">
+                                    <span class="card-title center meu-span green-text text-lighten-3"><?php echo $nome     ?></span>
+                                </div>
+                                <div class="card-action green darken-4">
+                                    <a class="green-text text-lighten-3" href="saibaRocha.php?idrocha=<?php echo $dados['idrocha']  ?>">Saiba mais</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                <?php } ?>
+                    <?php } ?>
+                </div>
             </div>
-        </div>
-        <br><br><br>
+            <br><br><br>
     </main>
     <?php
     include "footer.php";
