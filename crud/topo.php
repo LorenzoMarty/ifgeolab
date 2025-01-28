98923e450baa745763290d09cec71010bd493105
Lorenzo <lorenzo.2022310934@aluno.iffar.edu.br>
2025-01-28 00:14:16 -0300

    commit

--- crud/topo.php
+++ crud/topo.php
@@ -172,7 +172,7 @@ if (mysqli_num_rows($resultado) > 0) {
     <?php if ($_SESSION['permissao'] == 3): ?>
       <li><a href="listarUsuario.php">Usuários</a></li>
     <?php endif; ?>
-    <li><a href="forms.php?tipo=questionário">Questões</a></li>
+    <li><a href="listarQuestao.php">Questões</a></li>
     <li><a href="Sugestao.php">Sugestões</a></li>
     <li><a href="Amostra.php">Amostras</a></li>
   </ul>
